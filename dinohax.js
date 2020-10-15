478f48205007e05462973cc28e758c891a1d5220
Samozd <71620375+Samozd@users.noreply.github.com>
2020-10-14 23:36:14 -0400

    Update dinohax.js

--- dinohax.js
+++ dinohax.js
@@ -3,14 +3,16 @@ if (document.title != "chrome://dino/") {
         throw new Error('Wrong webpage');
 }
 document.title = "chrome://dino";
+const html = "<body><a>Thank you for using SamHack</a><br><br><a>Invincibility</a><input type=\"checkbox\"id=\"invin\"><br><a>Set Speed			</a><input type=\"text\" id=\"spd\" name=\"spd\"><br><button onclick=\"setspd()\"id=\"setspd\">Set Speed</button><br><br><a>Set High Score			</a><input type=\"text\" id=\"scr\" name=\"scr\"><br><button onclick=\"setshs()\"id=\"setshs\">Set High Score</button><br><br><a>Remove Cactus</a><input type=\"checkbox\"id=\"cac\"><br><button onclick=\"game0ver()\"id=\"endgame\">End Game</button><br><br><a>Stop Dino</a><input type=\"checkbox\"id=\"remv\"><br><br><a>Weird stuff</a><br><a>Set State			</a><br><button onclick=\"setsta('crashed')\"id=\"setstcr\">CRASHED</button><button onclick=\"setsta('ducking')\"id=\"setstdu\">DUCKING</button><button onclick=\"setsta('jumping')\"id=\"setstju\">JUMPING</button><button onclick=\"setsta('running')\"id=\"setstru\">RUNNING</button><button onclick=\"setsta('waiting')\"id=\"setstwa\">WAITING</button></body>";
 document
 .evaluate('//*[text()="ERR_INTERNET_DISCONNECTED"]', document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE)
 .snapshotItem(0)
 // begin html
-.innerHTML = "<body><a>Thank you for using SamHack</a><br><br><a>Invincibility</a><input type=\"checkbox\"id=\"invin\"><br><br><a>Set Speed			</a><input type=\"text\" id=\"spd\" name=\"spd\"><br><button onclick=\"setspd()\"id=\"setspd\">Set Speed</button><br><br><br><a>Set High Score			</a><input type=\"text\" id=\"scr\" name=\"scr\"><br><button onclick=\"setshs()\"id=\"setshs\">Set High Score</button><br><br><br><a>Remove Cactus</a><input type=\"checkbox\"id=\"cac\"><br><button onclick=\"game0ver()\"id=\"endgame\">End Game</button><br><br><br><a>Weird stuff</a><br><a>Set State			</a><br><button onclick=\"setsta('crashed')\"id=\"setstcr\">CRASHED</button><button onclick=\"setsta('ducking')\"id=\"setstdu\">DUCKING</button><button onclick=\"setsta('jumping')\"id=\"setstju\">JUMPING</button><button onclick=\"setsta('running')\"id=\"setstru\">RUNNING</button><button onclick=\"setsta('waiting')\"id=\"setstwa\">WAITING</button></body>";
+.innerHTML = html;
 // end html
 document.getElementById("invin").style.opacity = 100;
 document.getElementById("cac").style.opacity = 100;
+document.getElementById("remv").style.opacity = 100;
 document.getElementById("setspd").style.cssFloat = "left";
 document.getElementById("setshs").style.cssFloat = "left";
 document.getElementById("setstcr").style.cssFloat = "left";
@@ -22,8 +24,8 @@ document.getElementById("endgame").style.cssFloat = "left";
 
 // init above
 
-var old1 = false;
-var old2 = false;
+var old1;
+var old2;
 var old3 = false;
 var a = Runner.instance_.gameOver;
 var b = [Obstacle.types,Horizon.prototype.addNewObstacle,checkForCollision];
@@ -31,37 +33,23 @@ var b = [Obstacle.types,Horizon.prototype.addNewObstacle,checkForCollision];
 // vars above
 
 function setspd() {
-	if (isNaN(Number(document.getElementById("spd").value)) || document.getElementById("spd").value < 2) {
+	if (isNaN(Number(document.getElementById("spd").value)) || document.getElementById("spd").value < 1) {
 		document.getElementById("spd").value = "1";
-		alert('Value must be a number larger than 2.');
+		alert('Value must be a number larger than 1.');
 	} else {
-		Runner.instance_.setSpeed(document.getElementById("spd").value);
+		Runner.instance_.setSpeed(Number(document.getElementById("spd").value));
 	}
 }
 function setshs() {
-	if (isNaN(Number(document.getElementById("scr").value)) || (document.getElementById("scr").value < 1 || document.getElementById("scr").value > 99999)) {
+	if (isNaN(Number(document.getElementById("scr").value)) || (document.getElementById("scr").value < 1 || document.getElementById("scr").value > 999999)) {
 		document.getElementById("scr").value = "0";
-		alert('Value must be a number between 0 and 100,000.');
+		alert('Value must be a number between 0 and 1,000,000.');
 	} else {
 		Runner.instance_.saveHighScore(document.getElementById("scr").value*40);
 	}
 }
 function setsta(a) {
-	if (a == "crashed") {
-		Runner.instance_.tRex.update(100, Trex.status.CRASHED);
-	}
-	if (a == "ducking") {
-		Runner.instance_.tRex.update(100, Trex.status.DUCKING);
-	}
-	if (a == "jumping") {
-		Runner.instance_.tRex.update(100, Trex.status.JUMPING);
-	}
-	if (a == "running") {
-		Runner.instance_.tRex.update(100, Trex.status.RUNNING);
-	}
-	if (a == "waiting") {
-		Runner.instance_.tRex.update(100, Trex.status.WAITING);
-	}
+	Runner.instance_.tRex.update(100, a.toUpperCase());
 }
 
 // buttons above
@@ -88,6 +76,15 @@ setInterval(function (){
 			checkForCollision = b[2];
 		}
 		old2 = document.getElementById("cac").checked;
+    }
+    if (document.getElementById("remv").checked) {
+        Runner.instance_.setSpeed(0.01);
+        old3 = document.getElementById("remv").checked
+    } else {
+        if (old3 != document.getElementById("remv").checked) {
+            Runner.instance_.setSpeed(2);
+            old3 = document.getElementById("remv").checked
+        }
 	}
 })
 
